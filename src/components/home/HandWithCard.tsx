53b519a12523fc46f306c13bf59ac7a04d6d1618
Martins Ngene <martinsngene9@gmail.com>
2023-05-01 20:57:38 +0100

    update: create components and start home page

---
+++ src/components/home/HandWithCard.tsx
@@ -0,0 +1,11 @@
+/** @format */
+
+import React from "react";
+
+import styles from "./styles.module.css";
+
+const HandWithCard = () => {
+  return <div className={`${styles.container}`}></div>;
+};
+
+export default HandWithCard;
